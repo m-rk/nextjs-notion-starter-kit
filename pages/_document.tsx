636394a64519996b175aed3f0e476baf6b689a6b
Mark Davies <mark+github@tan.gl>
2023-05-23 22:45:36 +0800

    google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -64,6 +64,19 @@ export default class MyDocument extends Document {
             <Main />
 
             <NextScript />
+
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-H55RKF7DSK"></script>
+            <script
+      dangerouslySetInnerHTML={{
+        __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-H55RKF7DSK');
+`
+              }}
+            />
           </body>
         </Html>
       </IconContext.Provider>
